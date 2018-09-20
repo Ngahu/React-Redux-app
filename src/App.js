a4a6533ef35c028bb15372d0ae75dc24fc6df8d2
Ngahu <jamaalaraheem@gmail.com>
2018-09-20 19:14:05 +0300

    Working with redux thus creation of the actions reducers and the global store where the app's state will live

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-
-// import './App.css';
-
 import Posts  from './components/Posts';
-
 import PostsForm from './components/PostForm';
 
+import { Provider }  from  'react-redux';
 
+import  store from './store';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
 
       <PostsForm/>
         <Posts/>
       </div>
+
+      </Provider>
     );
   }
 }
