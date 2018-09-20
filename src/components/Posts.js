104b9a9c351432cd2b8fc8f3abafee273ddb9ea4
Ngahu <jamaalaraheem@gmail.com>
2018-09-20 14:30:08 +0300

    initial commit of the project

---
+++ src/components/Posts.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Posts extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Posts.propTypes = {
+
+};
+
+export default Posts;
