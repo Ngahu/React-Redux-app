102d5b8d4de2b46dc9507a3f74f6a1bc6544e63f
Ngahu <jamaalaraheem@gmail.com>
2018-09-23 02:29:26 +0300

    this is the last commit for the project for now.Created the production build of the project and also removed all console logs from the project

--- src/actions/PostActions.js
+++ src/actions/PostActions.js
@@ -19,7 +19,6 @@ export const fetchPosts = () => dispatch =>{
 
 
 export const createPost = (postData) => dispatch =>{
-    console.log("posting")
     fetch('https://jsonplaceholder.typicode.com/posts',{
         method: 'POST',
         headers:{
